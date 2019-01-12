fe4205b52beef43bfc6a76d03a802dffe743fb9e
Milexys <milexysbetancourt1@gmail.com>
2019-01-12 18:42:03 -0300

    Creando mi store

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const $form = document.getElementById('form');
 $form.addEventListener('submit', handleSubmit);
 
@@ -7,3 +9,30 @@ function handleSubmit(event) {
 	const title = data.get('title');
 	console.log(title);
 }
+
+const initialState = [
+	{
+		"title": "Despacito",
+	},
+	{
+		"title": "One more time",
+	},
+	{
+		"title": "Echame la culpa a mi",
+	}
+]
+
+const store = createStore(
+	(state) => state,
+	initialState,
+	 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const $container = document.getElementById('playlist');
+const playlist = store.getState();
+
+playlist.forEach((item) => {
+	const template = document.createElement('p');
+	template.textContent = item.title;
+	$container.appendChild(template);
+})
