8328bfd27eef30c384ef8102079afbba4ed64743
Milexys <milexysbetancourt1@gmail.com>
2019-01-13 00:21:17 -0300

    Actualizando el estado a través de un reducer

---
+++ src/reducers/data.js
@@ -0,0 +1,18 @@
+function data(state, action) {
+    switch(action.type) {
+        case 'SEARCH_VIDEO': {
+            const list = state.data.categories[2].playlist;
+            const results = list.filter((item) => {
+                return item.author.includes(action.payload.query)
+            })
+            return {
+                ...state,
+                search: results,
+            }
+        }
+        default:
+            return state
+    }
+} 
+
+export default data;
