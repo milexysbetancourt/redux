96ed2add73a9ca1f8448da1ca11165e540d9b4c8
Milexys <milexysbetancourt1@gmail.com>
2019-01-12 22:15:23 -0300

    Agregado un store dentro de mi módulo de react

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -40,7 +40,7 @@ const reducer = function(state, action) {
 const store = createStore(
 	reducer,
 	initialState,
-	 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 function render() {
